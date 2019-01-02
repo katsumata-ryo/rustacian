0ce464e0ebe76da3f2c177410b41305b40a5be09
Katsumata Ryo <katsumata@stores.jp>
2019-01-03 00:26:13 +0900

    引数部分を複数取れるようにした

--- exif/src/main.rs
+++ exif/src/main.rs
@@ -2,13 +2,15 @@ use std::env;
 use rexiv2;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let meta = rexiv2::Metadata::new_from_path(&file_path).unwrap();
+    let arguments = env::args().skip(1);
+    for argument in arguments {
+        let meta = rexiv2::Metadata::new_from_path(&argument).unwrap();
 
-    println!("Width: {:?}", meta.get_pixel_width());
-    println!("Height: {:?}", meta.get_pixel_height());
-    println!("F: {:?}", meta.get_fnumber().unwrap());
-    println!("ISO: {:?}", meta.get_iso_speed().unwrap());
-    println!("Shutter: {:?}/{:?}", meta.get_exposure_time().unwrap().numer(), meta.get_exposure_time().unwrap().denom());
+        println!("[{}]", argument);
+        println!("Width: {:?}", meta.get_pixel_width());
+        println!("Height: {:?}", meta.get_pixel_height());
+        println!("F: {:?}", meta.get_fnumber().unwrap());
+        println!("ISO: {:?}", meta.get_iso_speed().unwrap());
+        println!("Shutter: {:?}/{:?}", meta.get_exposure_time().unwrap().numer(), meta.get_exposure_time().unwrap().denom());
+    }
 }
